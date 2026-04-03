609b523013139c5602a65481fb51df5665f233f4
yogeswar2006 <yogi8247322760@gmail.com>
2026-04-03 12:19:51 +0530

    updated : STATIC ROOT

--- config/settings.py
+++ config/settings.py
@@ -193,9 +193,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = "media/"
 
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = '/callback/'
 
